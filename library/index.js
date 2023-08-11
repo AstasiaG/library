6f2aca2923590edf0a9b35e94cf8a5fe92bdb66e
Anastasia Glinkina <anstasa.li@gmail.com>
2023-08-11 16:01:47 +0400

    feat: add animation on burger menu

--- library/index.js
+++ library/index.js
@@ -5,10 +5,15 @@ const menuItems = document.querySelectorAll('li.list__item')
 burger.addEventListener('click', () => {
   burger.classList.toggle('burger-menu__active');
   menu.classList.toggle('transform');
+  menuItems.forEach(e => {
+    e.classList.toggle('list__item-active')
+  });
 })
 menuItems.forEach(e => {
-  e.addEventListener('click', () => {
+  e.addEventListener('click', (e) => {
     menu.classList.remove('transform');
+    burger.classList.remove('burger-menu__active');
+    e.classList.remove('list__item-active')
   })
 })
 
@@ -16,5 +21,9 @@ document.addEventListener('click', (el) => {
   const notMenu = el.composedPath().includes(burger);
   if(!notMenu) {
     menu.classList.remove('transform');
+    burger.classList.remove('burger-menu__active');
+    menuItems.forEach(e => {
+      e.classList.remove('list__item-active')
+    });
   }
 })
